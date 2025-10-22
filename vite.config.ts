6c2aa48e04f32740b4a36b34ffb330ecbe873245
Mahmoud Yassin <mahmoudabdelghaffar2008@gmail.com>
2025-10-22 18:45:12 +0300

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -8,6 +7,7 @@ const REPO_NAME = "Mahmoud-Yassin-Portfolio";
 
 export default defineConfig(({ mode }) => ({
   base: mode === "development" ? "/" : `/${REPO_NAME}/`,
+  build: { outDir: "docs" }, // <-- put build in /docs
   server: { host: "::", port: 8080 },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
